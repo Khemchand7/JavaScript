d6e6bd391ccfef1c0c80c0c75adb3fcefedb98a0
khemchand7 <khemchandk360@gmail.com>
2024-08-22 13:54:20 +0530

    remove multi line comments

--- Hoisting/script.js
+++ Hoisting/script.js
@@ -1,36 +1,36 @@
 // What is Hoisting?
 // Hoisting is phenomenon is JS by which you can access variables and functions even before you have initialized.
 
-/* //Scenario 1
+//Scenario 1
 var x = 7;
 function getName(){
   console.log("Namaste Javascrit")
 }
 getName();//function invocation will print Namaste Javascript
-console.log(x);// 7 */
+console.log(x);// 7
 
 //Scenario 2
 
-/* getName();//Namaste Javascript ==>kyuki memore allocation me pura function memory me tha
+getName();//Namaste Javascript ==>kyuki memore allocation me pura function memory me tha
 console.log(x);//Undefined ==> memory to allocate hui hai magar value initialized nhi hui
 console.log(getName)//ƒ getName(){console.log("Namaste Javascrit")}
 var x = 7;
 function getName(){
   console.log("Namaste Javascrit")
-} */
+}
 
 
 
-/* //Scenario 3
+//Scenario 3
 getName();// error => getName is not a function because arrow functions behave like a normal variable
 console.log(x);
 
 var x = 7;
 var getName = () => {
   console.log("Namaste Javascrit")
-} */
+}
 
-/*   //Scenario 4
+  //Scenario 4
 
 console.log(x);//Undefined
 console.log(getName);//undefined
@@ -39,7 +39,7 @@ getName();//error => getName is not a function because this function definition 
 var x = 7;
 var getName = function() {
   console.log("Namaste Javascrit")
-} */
+}
 
     //Scenario 4
 
